f97b7afd0adf504c54fd35205a91d5977d28264c
keelan02 <keohanekeelan2@gmail.com>
2025-06-07 19:11:17 +0200

    Mise à jour des chemins d'images dans projex.ts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
         <div className="flex items-center gap-5">
           <div className="rounded-full overflow-hidden w-16 h-16 md:w-20 md:h-20 flex-shrink-0">
             <img
-              src="/public/image.png"
+              src="/image.png"
               alt="Keelan Keohane"
               className="w-full h-full object-cover"
             />
