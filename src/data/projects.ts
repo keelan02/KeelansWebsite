9f5e0adaf95cc27fb1545d74a73a03d0934c7c99
keelan02 <keohanekeelan2@gmail.com>
2025-06-16 21:05:37 +0200

    Mise à jour CHRYSALIDE

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -17,27 +17,34 @@ export const projects: Project[] = [
   },
   {
     id: 3,
+    title: "Cadreur & monteur – Concert du groupe Chrysalide ",
+    thumbnailUrl: "/CHRYSALIDE",
+    videoUrl: "https://www.youtube.com/embed/BUI5faDoHFY",
+    description: "Captation live d’un groupe indépendant réalisée avec un Sony A7III, suivie d’une post-production complète incluant l’étalonnage avec LUT personnalisée, le montage, le mixage audio, ainsi que l’export d’extraits promotionnels destinés à la communication du groupe."
+  },
+  {
+    id: 4,
     title: "Monteur / Cadreur pour le clip expérimental de l'artiste PLUCKY (Février- Avril 2024)",
     thumbnailUrl: "/PLUCKY.png",
     videoUrl: "https://www.youtube.com/embed/ybCUJTtaC6M",
     description: "Pendant mon stage chez Orfilm à Paris, j’ai profité de mon temps libre pour réaliser un clip expérimental pour un ami artiste, Plucky. J’ai tourné avec mon Sony Alpha 7 III une série de plans variés, que j’ai ensuite montés sur Premiere Pro et After Effects."
   },
   {
-    id: 4,
+    id: 5,
     title: "Montage style Publicitaire pour un TD sur Avid Media Composer (Février 2024)",
     thumbnailUrl: "/AVID.png",
     videoUrl: "https://www.youtube.com/embed/mmtYWsbTCA4",
     description: "Monté dans le cadre d’un TD en 3ᵉ année de post-production à l’ESEC Lyon, à partir de rushs expérimentaux issus de la filière image des années précédentes. Inspiré par l’esthétique des clips publicitaires musicaux. Réalisé sur Avid Media Composer."
   },
   {
-    id: 5,
+    id: 6,
     title: "Motion Designer pour une vidéo promo d’un single musical (Mai 2023)",
     thumbnailUrl: "/SPIE.png",
     videoUrl: "https://www.youtube.com/embed/V6wMLfa8fOM",
     description: "À partir de la pochette illustrée du single, j'ai détouré l'image et animé l'ensemble sur After Effects."
   },
   {
-    id: 6,
+    id: 7,
     title: "Montage VFX / Montage en binôme sur le film PERCHÉ de Thibault Carrere (Mai 2023) ",
     thumbnailUrl: "/PERCHE.png",
     videoUrl: "https://www.youtube.com/embed/tJM_loO09LQ",
