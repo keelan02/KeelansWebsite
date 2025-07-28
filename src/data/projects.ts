311a01dc6acca3b2ed9733e6b6f02dc7a9c6fd35
keelan02 <keohanekeelan2@gmail.com>
2025-07-28 19:29:51 +0200

    ajout video

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,55 +3,62 @@ import { Project } from "../components/VideoCard";
 export const projects: Project[] = [
   {
     id: 1,
-    title: "Cadreur, monteur, réalisateur — Exercice de vidéo promotionnelle pour l’entreprise Würth (juin 2025)",
-    thumbnailUrl: "/WURTH.png",
-    videoUrl: "https://www.youtube.com/embed/vCDD3BCEHOU",
+    title: "Cadreur, monteur, réalisateur – vidéo promotionnelle de la startup Kosmolab (juillet 2025)",
+    thumbnailUrl: "/KOSMOLAB.png",
+    videoUrl: "https://youtube.com/shorts/-DqwSAOW5p0",
     description: "Sélectionné parmi 20 candidats sur 100 par une agence d’alternance, j’ai été chargé de concevoir une vidéo promotionnelle originale pour l’entreprise Würth, à réaliser en 3 jours. Le défi : produire une vidéo verticale, au format réseaux sociaux, de 30 secondes maximum, mettant en valeur les valeurs et l’impact de la marque, sans montrer directement les produits. J’ai imaginé le concept, organisé la session tournage, cadré les plans, monté la vidéo et assuré l’export pour un rendu adapté aux réseaux sociaux."
   },
   {
     id: 2,
+    title: "Cadreur, monteur, réalisateur — Exercice de vidéo promotionnelle pour l’entreprise Würth (juin 2025)",
+    thumbnailUrl: "/WURTH.png",
+    videoUrl: "https://www.youtube.com/embed/vCDD3BCEHOU",
+    description: "J'ai écrit, réalisé, cadré et monté une vidéo promotionnelle destinée aux réseaux sociaux pour Kosmolab, une startup innovante. Le projet a été entièrement mené sur un week-end, de la conception à la post-production."
+  },
+  {
+    id: 3,
     title: "Motion Design / Montage – Salon du Vin Bio (Avril 2025)",
     thumbnailUrl: "/CORPO.png",
     videoUrl: "https://www.youtube.com/embed/P2AMwvmwIt8",
     description: "Montage d’archives et création graphique d’un verre de vin modélisé sur Illustrator, puis animé sur After Effects. J'ai également animé les éléments graphiques (logo, dessin) fournis par le client."
   },
   {
-    id: 3,
+    id: 4,
     title: "Signature Animée et montée pour L'ESEC Lyon (Mai 2024)",
     thumbnailUrl: "/Signature.PNG",
     videoUrl: "https://www.youtube.com/embed/2MXgswyGQdw",
     description: "En 3ᵉ année à l’ESEC Lyon, filière post-production, j’ai réalisé une signature animée à partir des assets du logo ESEC, dessinée et animée sur Krita, puis montée dans Premiere Pro avec une transition fluide vers le logo."
   },
   {
-    id: 4,
+    id: 5,
     title: "Cadreur & monteur – Concert du groupe Chrysalide ",
     thumbnailUrl: "/CHRYSALIDE.png",
     videoUrl: "https://www.youtube.com/embed/BUI5faDoHFY",
     description: "Captation live d’un groupe indépendant réalisée avec un Sony A7III, suivie d’une post-production complète incluant l’étalonnage avec LUT personnalisée, le montage, le mixage audio, ainsi que l’export d’extraits promotionnels destinés à la communication du groupe."
   },
   {
-    id: 5,
+    id: 6,
     title: "Monteur / Cadreur pour le clip expérimental de l'artiste PLUCKY (Février- Avril 2024)",
     thumbnailUrl: "/PLUCKY.png",
     videoUrl: "https://www.youtube.com/embed/ybCUJTtaC6M",
     description: "Pendant mon stage chez Orfilm à Paris, j’ai profité de mon temps libre pour réaliser un clip expérimental pour un ami artiste, Plucky. J’ai tourné avec mon Sony Alpha 7 III une série de plans variés, que j’ai ensuite montés sur Premiere Pro et After Effects."
   },
   {
-    id: 6,
+    id: 7,
     title: "Montage style Publicitaire pour un TD sur Avid Media Composer (Février 2024)",
     thumbnailUrl: "/AVID.png",
     videoUrl: "https://www.youtube.com/embed/mmtYWsbTCA4",
     description: "Monté dans le cadre d’un TD en 3ᵉ année de post-production à l’ESEC Lyon, à partir de rushs expérimentaux issus de la filière image des années précédentes. Inspiré par l’esthétique des clips publicitaires musicaux. Réalisé sur Avid Media Composer."
   },
   {
-    id: 7,
+    id: 8,
     title: "Motion Designer pour une vidéo promo d’un single musical (Mai 2023)",
     thumbnailUrl: "/SPIE.png",
     videoUrl: "https://www.youtube.com/embed/V6wMLfa8fOM",
     description: "À partir de la pochette illustrée du single, j'ai détouré l'image et animé l'ensemble sur After Effects."
   },
   {
-    id: 8,
+    id: 9,
     title: "Montage VFX / Montage en binôme sur le film PERCHÉ de Thibault Carrere (Mai 2023) ",
     thumbnailUrl: "/PERCHE.png",
     videoUrl: "https://www.youtube.com/embed/tJM_loO09LQ",
